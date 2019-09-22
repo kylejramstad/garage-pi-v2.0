17be573ba710d2f9818628060ca7108e6ea3604f
Kyle Ramstad <kylejramstad@gmail.com>
2019-09-21 17:19:26 -0700

    Update and Install new Packages
    Install new packages if they have been added to package.json from updating from github.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+# Get latest code from github
 cd ..
 git -c user.name=test -c user.email=test@test.com stash
 git pull
 git stash pop
+
+# Installs all modules in package.json and checks for security issues and fix them
+sudo npm install
+sudo npm audit fix
+
+# Run webpack so that the bundle.js can be created
+npx webpack --config webpack.config.js
