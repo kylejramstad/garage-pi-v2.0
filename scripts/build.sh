98568e73c7e00c4608b9ab2321258e611a9f6e4d
Kyle Ramstad <kylejramstad@gmail.com>
2019-11-03 19:16:52 -0800

    Disable update system
    It is not working. Don't enable until it is all worked out.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ./garage-pi-v2/ssl
 ip=$(host myip.opendns.com 208.67.220.222 | grep -oP "^myip\.opendns\.com.* \K(\d{1,3}\.){3}(\d{1,3})")
-sudo ./create.sh $ip $2 $3 $4 $5 $6 $7 $8
+sudo ./create.sh $ip $1 $2 $3 $4 $5 $6 $7
 cd ..
 sudo docker build -t bugman000/garage-pi-v2 .
 sudo docker run --restart=always --device=/dev/mem:/dev/mem --name=garage-pi --privileged --publish 443:443 -d bugman000/garage-pi-v2
-(sudo crontab -l 2>/dev/null; echo "* 3 * * * sudo docker exec garage-pi /bin/bash /code/scripts/update.sh && sudo docker container restart garage-pi") | sudo crontab -
+#(sudo crontab -l 2>/dev/null; echo "* 3 * * * sudo docker exec garage-pi /bin/bash /code/scripts/update.sh && sudo docker container restart garage-pi") | sudo crontab -
--- scripts/download.sh
+++ scripts/download.sh
@@ -3,4 +3,4 @@ sudo docker run --restart=always --device=/dev/mem:/dev/mem --name=garage-pi --p
 ip=$(host myip.opendns.com 208.67.220.222 | grep -oP "^myip\.opendns\.com.* \K(\d{1,3}\.){3}(\d{1,3})")
 sleep 1m
 sudo docker exec -i garage-pi /bin/sh -c "cd /code/ssl;./create.sh $ip $1 $2 $3 $4 $5 $6 $7" && sudo docker container restart garage-pi
-(sudo crontab -l 2>/dev/null; echo "* 3 * * * sudo docker exec garage-pi /bin/bash /code/scripts/update.sh && sudo docker container restart garage-pi") | sudo crontab -
+#(sudo crontab -l 2>/dev/null; echo "* 3 * * * sudo docker exec garage-pi /bin/bash /code/scripts/update.sh && sudo docker container restart garage-pi") | sudo crontab -
