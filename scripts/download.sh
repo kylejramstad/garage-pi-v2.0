27954a6e39e2786ddc58af79d30e9a14ab76b7e9
Kyle Ramstad <kylejramstad@gmail.com>
2019-12-01 20:29:58 -0800

    Fix error

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,8 +1,3 @@
 curl -sSL https://get.docker.com | sh
 sudo docker run --restart=always --device=/dev/mem:/dev/mem --name=garage-pi --privileged --publish 443:443 --publish 80:80 -d bugman000/garage-pi-v2
-sudo docker exec -i garage-pi certbot certonly --webroot -w /code/tls -n --domains $1 --agree-tos --email $2
-sudo docker exec -i garage-pi sudo rm /code/tls/fullchain.pem
-sudo docker exec -i garage-pi sudo rm /code/tls/privkey.pem
-sudo docker exec -i garage-pi ln -s /etc/letsencrypt/live/$1/fullchain.pem /code/tls/fullchain.pem
-sudo docker exec -i garage-pi ln -s /etc/letsencrypt/live/$1/privkey.pem /code/tls/privkey.pem
-sudo docker container restart garage-pi
+sudo docker exec -i garage-pi certbot certonly --webroot -w /code/tls -n --domains $1 --agree-tos --email $2 && sudo docker exec -i garage-pi sudo rm /code/tls/fullchain.pem && sudo docker exec -i garage-pi sudo rm /code/tls/privkey.pem && sudo docker exec -i garage-pi ln -s /etc/letsencrypt/live/$1/fullchain.pem /code/tls/fullchain.pem && sudo docker exec -i garage-pi ln -s /etc/letsencrypt/live/$1/privkey.pem /code/tls/privkey.pem && sudo docker container restart garage-pi
