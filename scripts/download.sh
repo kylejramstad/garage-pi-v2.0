2ae8593d423e6fcb9a044b174a74d07ef1724b2f
Kyle Ramstad <kylejramstad@gmail.com>
2020-01-05 13:13:41 -0800

    Update after downloading

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,4 +1,5 @@
 curl -sSL https://get.docker.com | sh
 sudo docker run --restart=always --device=/dev/mem:/dev/mem --name=garage-pi --privileged --publish 443:443 --publish 80:80 -d bugman000/garage-pi-v2
 sudo docker exec -i garage-pi certbot certonly --webroot -w /code/tls -n --domains $1 --agree-tos --email $2 && sudo docker exec -i garage-pi sudo rm /code/tls/fullchain.pem && sudo docker exec -i garage-pi sudo rm /code/tls/privkey.pem && sudo docker exec -i garage-pi ln -s /etc/letsencrypt/live/$1/fullchain.pem /code/tls/fullchain.pem && sudo docker exec -i garage-pi ln -s /etc/letsencrypt/live/$1/privkey.pem /code/tls/privkey.pem && sudo docker container restart garage-pi
+sudo docker exec -t garage-pi /code/scripts/update.sh && sudo docker container restart garage-pi
 sudo crontab -l > garage-pi-cron && echo "0 2 * * 3 sudo docker exec -t garage-pi /code/scripts/update.sh && sudo docker container restart garage-pi >/dev/null 2>&1" >> garage-pi-cron && sudo crontab garage-pi-cron && sudo rm garage-pi-cron
